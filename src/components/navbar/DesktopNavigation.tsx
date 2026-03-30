917e6d4fcb9d531000d8ef3360cfb63bafccc4c8
Tech4Humanity <62584578+TML-4PM@users.noreply.github.com>
2026-03-31 03:06:27 +1100

    nav: Community link → /community

--- src/components/navbar/DesktopNavigation.tsx
+++ src/components/navbar/DesktopNavigation.tsx
@@ -17,7 +17,7 @@ const DesktopNavigation = () => {
       <Link to="/store" className="inline-flex items-center px-1 pt-1 border-b-2 border-white text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300">
         Store
       </Link>
-      <Link to="/social" className="inline-flex items-center px-1 pt-1 border-b-2 border-white text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300">
+      <Link to="/community" className="inline-flex items-center px-1 pt-1 border-b-2 border-white text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300">
         Community
       </Link>
     </div>
