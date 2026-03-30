ad37e269cf43aa6ac8da7bcdfbe117f470190602
Tech4Humanity <62584578+TML-4PM@users.noreply.github.com>
2026-03-31 03:06:01 +1100

    community: add /community page route wrapping Community component

---
+++ src/pages/CommunityPage.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import Community from '@/components/Community';
+
+const CommunityPage = () => {
+  return (
+    <div className="pt-16">
+      <Community />
+    </div>
+  );
+};
+
+export default CommunityPage;
